350bd5b197d4c51f584e252ca38dbe0f6c634f56
lukedeo <lukepercivaldeoliveira@gmail.com>
2015-03-02 18:19:05 -0800

    formatting

--- jetconverter.py
+++ jetconverter.py
@@ -6,37 +6,37 @@ import numpy as np
 import sys
 
 _bufdtype = [('image', 'float64', (25, 25)), 
-		     ('signal', float),
-		     ('jet_pt', float),
-		     ('jet_eta', float),
-		     ('jet_phi', float)]
+             ('signal', float),
+             ('jet_pt', float),
+             ('jet_eta', float),
+             ('jet_phi', float)]
 
 def buffer_to_jet(entry, tag = 0):
-	"""
-	Takes an entry from an ndarray, and a tag = {0, 1} indicating
-	if its a signal entry or not.
-	"""
-	image = flip_jet(rotate_jet(np.array(entry['Intensity']), -entry['RotationAngle']))
-	return (image, tag, entry['LeadingPt'], entry['LeadingEta'], entry['LeadingPhi'])
+    """
+    Takes an entry from an ndarray, and a tag = {0, 1} indicating
+    if its a signal entry or not.
+    """
+    image = flip_jet(rotate_jet(np.array(entry['Intensity']), -entry['RotationAngle']))
+    return (image, tag, entry['LeadingPt'], entry['LeadingEta'], entry['LeadingPhi'])
 
 
 def is_signal(f, matcher = 'Wprime'):
-	key = matcher.lower().replace(' ', '').replace('-', '')
-	if key in f.lower().replace(' ', '').replace('-', ''):
-		return 1.0
-	return 0.0
+    key = matcher.lower().replace(' ', '').replace('-', '')
+    if key in f.lower().replace(' ', '').replace('-', ''):
+        return 1.0
+    return 0.0
 
 if __name__ == '__main__':
-	
-	entries = []
-	for fname in sys.argv[1:]:
-		print 'Working on file: {}'.format(fname)
-		with root_open(fname) as f:
-			df = f.EventTree.to_array()
-			tag = is_signal(fname, 'wprime')
-			for jet in df:
-				entries.append(buffer_to_jet(jet, tag))
+    
+    entries = []
+    for fname in sys.argv[1:]:
+        print 'Working on file: {}'.format(fname)
+        with root_open(fname) as f:
+            df = f.EventTree.to_array()
+            tag = is_signal(fname, 'wprime')
+            for jet in df:
+                entries.append(buffer_to_jet(jet, tag))
 
-	df = np.array(entries, dtype=_bufdtype)
+    df = np.array(entries, dtype=_bufdtype)
 
-	np.save('test.npy', x)
+    np.save('test.npy', x)
--- jettools.py
+++ jettools.py
@@ -4,53 +4,53 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 def rotate_jet(jet, angle, in_radians = True, normalizer = 1450, dim=25):
-	"""
-	Take an *flat* unrotated in the form of an array from MI.exe, and an angle, 
-	and rotates the jet to that angle using a
-	"""
-	im = jet.reshape((dim, dim))
-	im = np.flipud(im.T) / normalizer
+    """
+    Take an *flat* unrotated in the form of an array from MI.exe, and an angle, 
+    and rotates the jet to that angle using a
+    """
+    im = jet.reshape((dim, dim))
+    im = np.flipud(im.T) / normalizer
 
-	if in_radians:
-		angle = np.rad2deg(angle)
+    if in_radians:
+        angle = np.rad2deg(angle)
 
-	return sk.rotate(im, angle, order = 3)
+    return sk.rotate(im, angle, order = 3)
 
 
 def flip_jet(jet, pool = 'r'):
-	"""
-	Takes a rotated jet (25, 25) from rotate_jet() and flips it across the
-	vertical axis according to whether you want the right side
-	(pool = {r, R, right, Right, ...}) or the 
-	left side (pool = {l, L, Left, left, ...}) to contain the most energy.
-	"""
-	weight = jet.sum(axis = 0)
+    """
+    Takes a rotated jet (25, 25) from rotate_jet() and flips it across the
+    vertical axis according to whether you want the right side
+    (pool = {r, R, right, Right, ...}) or the 
+    left side (pool = {l, L, Left, left, ...}) to contain the most energy.
+    """
+    weight = jet.sum(axis = 0)
 
-	halfway = jet.shape[0] / 2.
-	l, r = np.int(np.floor(halfway)), np.int(np.ceil(halfway))
-	l_weight, r_weight = np.sum(weight[:l]), np.sum(weight[r:])
+    halfway = jet.shape[0] / 2.
+    l, r = np.int(np.floor(halfway)), np.int(np.ceil(halfway))
+    l_weight, r_weight = np.sum(weight[:l]), np.sum(weight[r:])
 
-	if ('r' in pool.lower()) and ('l' in pool.lower()):
-		raise ValueError('Jet pooling side must have l -OR- r in the name.')
-	if 'r' in pool.lower():
-		if r_weight > l_weight:
-			return jet
-		return np.fliplr(jet)
-	elif 'l' in pool.lower():
-		if l_weight > r_weight:
-			return jet
-		return np.fliplr(jet)
-	else:
-		raise ValueError('Jet pooling side must have l -OR- r in the name.')
+    if ('r' in pool.lower()) and ('l' in pool.lower()):
+        raise ValueError('Jet pooling side must have l -OR- r in the name.')
+    if 'r' in pool.lower():
+        if r_weight > l_weight:
+            return jet
+        return np.fliplr(jet)
+    elif 'l' in pool.lower():
+        if l_weight > r_weight:
+            return jet
+        return np.fliplr(jet)
+    else:
+        raise ValueError('Jet pooling side must have l -OR- r in the name.')
 
 
 def plot_mean_jet(rec, field = 'image', title = 'Average Jet Image'):
-	fig = plt.figure(figsize=(8, 8), dpi=100)
-	ax = fig.add_subplot(111)
-	im = ax.imshow(np.mean(rec[field], axis = 0),  norm=LogNorm(vmin=0.00001, vmax=1), interpolation='nearest')
-	plt.title(r''+title)
-	return fig
-	
+    fig = plt.figure(figsize=(8, 8), dpi=100)
+    ax = fig.add_subplot(111)
+    im = ax.imshow(np.mean(rec[field], axis = 0),  norm=LogNorm(vmin=0.00001, vmax=1), interpolation='nearest')
+    plt.title(r''+title)
+    return fig
+    
 
 
 # x = np.array((rotate_jet(j['Intensity'], -j['RotationAngle']), j['LeadingPt']) for j in wprime, dtype=[('image', 'float64', (25, 25)), ('jet_pt',float)])
