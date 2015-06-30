9117a0a04efc9ea96e5346c77a957bb9709dc6a7
lukedeo <lukepercivaldeoliveira@gmail.com>
2015-06-30 11:18:53 +0200

    update setup

--- event-gen/setup.sh
+++ event-gen/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 setup_PYTHIA() {
-    export PYTHIA8LOCATION=/u/at/pnef/Work/Code/pythia8183/
-    export PYTHIA8DATA=${PYTHIA8LOCATION}xmldoc/
-    export LD_LIBRARY_PATH=${PYTHIA8LOCATION}lib/:$LD_LIBRARY_PATH
+    #export PYTHIA8LOCATION=/u/at/pnef/Work/Code/pythia8183/
+    export PYTHIA8LOCATION=/u/at/lukedeo/software/pythia8210
+    export PYTHIA8DATA=${PYTHIA8LOCATION}/xmldoc/
+    export LD_LIBRARY_PATH=${PYTHIA8LOCATION}/lib/:$LD_LIBRARY_PATH
 }
 
 setup_ROOT() {
@@ -11,8 +12,8 @@ setup_ROOT() {
 }
 
 setup_fastjet() {
-	export FASTJETLOCATION=/u/at/pnef/Work/Code/TrackBasedGrooming/stable/fastjet-3.0.3/fastjet-install/
-	#export FASTJETLOCATION=/u/at/pnef/Work/Code/TrackBasedGrooming/fastjet-3.0.3/fastjet-install/
+    export FASTJETLOCATION=/u/at/pnef/Work/Code/TrackBasedGrooming/stable/fastjet-3.0.3/fastjet-install/
+    #export FASTJETLOCATION=/u/at/pnef/Work/Code/TrackBasedGrooming/fastjet-3.0.3/fastjet-install/
     #export FASTJETLOCATION=/u/at/pnef/Work/Code/fastjet-install/
     export LD_LIBRARY_PATH=${FASTJETPATH}lib/:$LD_LIBRARY_PATH
 }
@@ -21,9 +22,13 @@ setup_boost() {
     export BOOSTINCDIR=/usr/include
     export BOOSTLIBLOCATION=/usr/lib64
 }
+setup_atlint() {
+    export PATH+=:$PYTHIA8LOCATION/bin
+    export PATH+=:$FASTJETLOCATION/bin
+}
 
 setup_ROOT
 setup_PYTHIA
 setup_fastjet
 setup_boost
-
+setup_atlint
