2ee115f8108736931d1bd270e988ed065164d76a
lukedeo <lukepercivaldeoliveira@gmail.com>
2015-07-01 17:08:12 +0200

    refactor python

---
+++ jettools/processing.py
@@ -0,0 +1,43 @@
+'''
+processing.py
+author: Luke de Oliveira, July 2015 
+
+Simple utilities for processing the junk that comes out of the ntuple event generation.
+'''
+
+import numpy as np
+from .jettools import rotate_jet, flip_jet, plot_mean_jet
+
+def buffer_to_jet(entry, tag = 0, side = 'r', max_entry = 2000, pix = 25):
+    """
+    Takes an *single* entry from an structured ndarray, i.e., X[i], 
+    and a tag = {0, 1} indicating if its a signal entry or not. 
+    The parameter 'side' indicates which side of the final 
+    jet image we want the highest energy.
+
+    The `entry` must have the following fields (as produced by event-gen)
+        * Intensity
+        * RotationAngle
+        * LeadingPt
+        * LeadingEta
+        * LeadingPhi
+        * LeadingM
+        * Tau32
+        * Tau21
+    """
+
+    image = flip_jet(rotate_jet(np.array(entry['Intensity']), -entry['RotationAngle'], normalizer=max_entry, dim=pix), side)
+    return (image / np.linalg.norm(image), tag, 
+        entry['LeadingPt'], entry['LeadingEta'], entry['LeadingPhi'], entry['LeadingM'], entry['Tau32'], entry['Tau21'])
+
+
+def is_signal(f, matcher = 'Wprime'):
+    """
+    Takes as input a filename and a string to match. If the 
+    'matcher' string is found in the filename, the file is 
+    taken to be a signal file.
+    """
+    key = matcher.lower().replace(' ', '').replace('-', '')
+    if key in f.lower().replace(' ', '').replace('-', ''):
+        return 1.0
+    return 0.0
